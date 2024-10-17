33c7d9a574dec65d053c32a1d9577203bbc9e096
rainingchicken <154983686+rainingchicken@users.noreply.github.com>
2024-10-16 18:53:23 -0700

    GET and POST home

--- server.mjs
+++ server.mjs
@@ -1,3 +1,4 @@
+// import dependencies
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
@@ -6,12 +7,42 @@ dotenv.config();
 import connectToMongo from "./database/connectToMongo.mjs";
 connectToMongo();
 
+//import model
+import FavSong from "./models/favSong.mjs";
+
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ message: "hello" });
+// to parse body json / understand what the client wants/sending
+app.use(express.json());
+
+//get all favsongs posted by client
+//read
+app.get("/", async (req, res) => {
+  try {
+    const allFavSongs = await FavSong.find({});
+    res.status(200).json(allFavSongs);
+  } catch (error) {
+    console.log("cannot get fav songs", error);
+  }
+});
+
+//create
+//POST - post your fav song
+app.post("/", async (req, res) => {
+  const { artist, songTitle } = req.body;
+  try {
+    const newFavSong = await FavSong.create({
+      // artist: artist,
+      // songTitle: songTitle
+      artist,
+      songTitle,
+    });
+    res.status(200).json(newFavSong);
+  } catch (error) {
+    console.log("bruh you did someting wrong", error);
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
